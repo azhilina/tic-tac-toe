5394e5697f4891a51916a01a8eb35e820d215219
Anna Zhilina <azhilina@tradingview.com>
2023-01-20 22:03:39 +0300

    stage3

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,29 +1,18 @@
 # write your code here
+
+import math
+
 line = input()
-# line = 'XXXOO__O_'
-# line1 = 'XOXOXOXXO'
-# line2 = 'XOOOXOXXO'
-# line3 = 'XOXOOXXXO'
-# line4 = 'XO_OOX_X_'
-# line5 = 'XO_XO_XOX'
-# line6 = '_O_X__X_X'
-# line7 = '_OOOO_X_X'
-# print(line)
+# line = 'XOXOOXXXO'
 
 print('---------')
 i = 0
 while i < 9:
-   print('|',line[i],line[i + 1],line[i + 2],'|')
-   i = i + 3
+    print('|',line[i],line[i + 1],line[i + 2],'|')
+    i = i + 3
 print('---------')
 
-def place(c,inp):
-    cond = False
-    number = 3 * (int(c[0]) - 1) + (int(c[1]) - 1)
-    if (inp[number] == "X" or inp[number] == 'O'):
-        cond = True
-    return cond
-    
+# this function returns the number of user inputs on the grid
 def nums(a, input):
     new_line = [1 if n == a else 0 for n in input]
     number = 0
@@ -31,34 +20,35 @@ def nums(a, input):
         number = number + i
     return number
     
-step = True
-while(step):
-    k = input().split(' ')
-    if (k[0] in ['1','2','3'] and k[1] in ['1','2','3']):
-        if (not place(k, line)):
-            step = False
-            number = 3 * (int(k[0]) - 1) + (int(k[1]) - 1)
-            exit_line = []
-            l = 0
-            while (l < 9):
-                if l == number:
-                    exit_line.append('X')
-                    l = l + 1
-                else:
-                    exit_line.append(line[l])
-                    l = l + 1
-        else:
-            print('This cell is occupied! Choose another one!')
-    elif (k[0].isdigit() and k[1].isdigit()):
-        print('Coordinates should be from 1 to 3!')
-    else:
-        print('You should enter numbers!')
+x = nums('X',line)
+o = nums('O',line)
+l = math.fabs(nums('X',line)-nums('O',line))
 
+# this function returns the winner
+def win(a, input):
+    nl = [1 if n == a else 0 for n in input]
+    if ((nl[0] + nl[1] + nl[2] == 3) or
+    (nl[3] + nl[4] + nl[5] == 3) or
+    (nl[6] + nl[7] + nl[8] == 3) or
+    (nl[0] + nl[3] + nl[6] == 3) or
+    (nl[1] + nl[4] + nl[7] == 3) or
+    (nl[2] + nl[5] + nl[8] == 3) or
+    (nl[0] + nl[4] + nl[8] == 3) or
+    (nl[2] + nl[4] + nl[6] == 3)):
+        return True
 
-print('---------')
-i = 0
-while i < 9:
-   print('|',exit_line[i],exit_line[i + 1],exit_line[i + 2],'|')
-   i = i + 3
-print('---------')
-    
+win('X', line)
+win('O', line)
+
+if (l > 1):
+    print('Impossible')
+elif(win('X', line) and win('O', line)):
+    print('Impossible')
+elif (win('X', line)):
+    print('X','wins')
+elif (win('O', line)):
+    print('O','wins')
+elif ((x + o) < 9):
+    print('Game not finished')
+elif ((x + o) == 9):
+    print('Draw')
